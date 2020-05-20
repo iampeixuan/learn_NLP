0bc11c6b83f0bcaa53976ab731b645c3c1e23f06
peixuan <sinpx@outlook.com>
2020-05-20 16:00:32 +0800

    convert keras h5 to tf pb model

---
+++ tensorflow/mnist.py
@@ -0,0 +1,34 @@
+def get_mnist():
+    import keras
+    from keras import backend as K
+    from keras.datasets import mnist
+
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+    num_classes = 10
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
+    # convert class vectors to binary class matrices
+    y_train_cat = keras.utils.to_categorical(y_train, num_classes)
+    y_test_cat = keras.utils.to_categorical(y_test, num_classes)
+
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+
+    return input_shape, num_classes, x_train, y_train, y_train_cat, x_test, y_test, y_test_cat 
